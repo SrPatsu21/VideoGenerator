115067d735626f69c838bd56ad88364d5b468733
patsu21not <paulosergiopierdona@gmail.com>
2025-11-04 10:15:35 -0300

    little fix

--- src/client/image/stb_image_impl.cpp
+++ src/client/image/stb_image_impl.cpp
@@ -1,5 +1,5 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include stb_image.h
+#include "stb_image.h"
 
 unsigned char* loadTexture(const char* filename, int* width, int* height, int* channels) {
     return stbi_load(filename, width, height, channels, STBI_rgb_alpha);
